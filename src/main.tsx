9c7ed55a244c9aba2a1b2f16e145b8a1c039d049
UdhvaPatel <120543667+UdhvaPatel@users.noreply.github.com>
2025-03-25 13:48:17 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,17 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.tsx'
+// import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+// createRoot(document.getElementById("root")!).render(<App />);
+import { createRoot } from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
+import App from './App.tsx';
+import './index.css';
+
+createRoot(document.getElementById('root')!).render(
+  <>
+    <App />
+    <Analytics />
+  </>
+);
+
