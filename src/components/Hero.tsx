23f328f53a679d26bee429884bc89b3b457200dc
UdhvaPatel <120543667+UdhvaPatel@users.noreply.github.com>
2026-04-19 16:01:21 -0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -92,12 +92,12 @@ const Hero = () => {
             >
               <Terminal className="inline-block mr-2 h-4 w-4" /> Get In Touch
             </a>
-            {/* <a
+             {/* <a
               // onClick={handleResumeClick}
               className="px-6 py-3 bg-tech-purple/90 text-white rounded-lg hover:shadow-glow-purple transition-all duration-300 cyber-border cursor-pointer"
-            >
+            > */}
               {/* <FileText className="inline-block mr-2 h-4 w-4" /> View Resume */}
-            </a> */}
+            {/* </a> */} 
           </div>
         </div>
       </div>
